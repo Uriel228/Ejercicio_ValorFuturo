eaa5f806ff0782c3c4799eef3be0c8d78c5906bf
Yasu <uriyasu10@gmail.com>
2021-10-19 01:42:28 -0500

    last

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-let VP = document.getElementById("VP"); //Valor presente
-let VF = document.getElementById("VF"); //Valor futuro
-let I = document.getElementById("I"); //Intereses
-let N = document.getElementById("N"); //Numero de periodos
-
-function valorFuturo(VP, I, N){
-    let subtotal = VP * (1 + I)
-    let total = Math.pow(subtotal, N)
-    return total
+function calcularvf(){
+var VP = document.getElementById("VP").value //Valor presente
+var VF = document.getElementById("VF").value //Valor futuro
+var I = document.getElementById("I").value //Intereses
+var N = document.getElementById("N").value //Numero de periodos
+let total = parseFloat(VP) *     Math.pow( 1 + parseFloat(I)/100, parseFloat(N));
+total = total.toFixed(2)
+document.getElementById("resultadovf").value = total;
 }
