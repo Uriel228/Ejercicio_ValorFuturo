42c5b3071a05a7701786c9e5b5fa5e1aff49af01
Yasu <uriyasu10@gmail.com>
2021-10-19 04:46:11 -0500

    Finalizado el reto

--- index.js
+++ index.js
@@ -25,3 +25,12 @@ function calculari(){
     total = Math.round(total)
     document.getElementById("resultadoi").value = total
 }
+function calcularn(){
+    var VP = document.getElementById("VP").value //Valor presente
+    var VF = document.getElementById("VF").value //Valor futuro
+    var I = document.getElementById("I").value //Intereses
+    var N = document.getElementById("N").value //Numero de periodos
+    let total = (Math.log(VF/VP)/Math.log(1+I/100))
+    total = Math.round(total)
+    document.getElementById("resultadon").value = total
+}
