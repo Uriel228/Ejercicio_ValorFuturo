97f8d1246e234955634731951a747cbaaa4b8627
Yasu <uriyasu10@gmail.com>
2021-10-19 04:34:11 -0500

    Agregando interes

--- index.js
+++ index.js
@@ -3,7 +3,25 @@ var VP = document.getElementById("VP").value //Valor presente
 var VF = document.getElementById("VF").value //Valor futuro
 var I = document.getElementById("I").value //Intereses
 var N = document.getElementById("N").value //Numero de periodos
-let total = parseFloat(VP) *     Math.pow( 1 + parseFloat(I)/100, parseFloat(N));
+let total = parseFloat(VP) * Math.pow( 1 + parseFloat(I)/100, parseFloat(N));
 total = total.toFixed(2)
 document.getElementById("resultadovf").value = total;
+}
+function calcularvi(){
+var VP = document.getElementById("VP").value //Valor presente
+var VF = document.getElementById("VF").value //Valor futuro
+var I = document.getElementById("I").value //Intereses
+var N = document.getElementById("N").value //Numero de periodos
+let total = VF / (1 + (I/100) * N )
+total = Math.round(total)
+document.getElementById("resultadovi").value = total;
+}
+function calculari(){
+    var VP = document.getElementById("VP").value //Valor presente
+    var VF = document.getElementById("VF").value //Valor futuro
+    var I = document.getElementById("I").value //Intereses
+    var N = document.getElementById("N").value //Numero de periodos
+    let total = (Math.pow(VF / VP, 1/N ) - 1)*100
+    total = Math.round(total)
+    document.getElementById("resultadoi").value = total
 }
