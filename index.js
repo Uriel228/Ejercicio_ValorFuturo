9618ee6cdcf61e3d84b98ca33460da7cce23d61a
Yasu <uriyasu10@gmail.com>
2021-10-18 19:01:25 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let valorPresente = 0; //VP
+let valorFuturo = 0; //VF
+let interes = 0; //I
+let periodos = 0; //N
+
+function valorFuturo(){
+    valorPresente = valorPresente*(1+interes)
+    valorPresente = Math.pow(periodos,2)
+}
